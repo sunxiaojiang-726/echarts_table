f46d61cbc29c0e8bd80af91b1bf61365049cc4ea
itheima <itheima@itcast.cn>
2022-06-24 13:59:55 +0800

    修改

--- js/flexible.js
+++ js/flexible.js
@@ -1,18 +1,24 @@
-(function flexible(window, document) {
+;(function flexible(window, document) {
+  // 获取html的根元素
   var docEl = document.documentElement
+  // dpr物理像素比
+  // 如果当前浏览器有物理像素比(比如移动端,iphone6-8的dpr为2),则直接拿到,否则就按照1处理(pc端dpr为1)
   var dpr = window.devicePixelRatio || 1
 
-  // adjust body font size
+  // 设置body的字体大小
   function setBodyFontSize() {
+    // 如果页面中有body这个元素,就设置body字体大小
     if (document.body) {
-      document.body.style.fontSize = (12 * dpr) + 'px'
-    }
-    else {
+      document.body.style.fontSize = 12 * dpr + 'px'
+    } else {
+      // 如果页面中没有body这个元素(因为如果在病毒引入该flexible.js文件,此时DOM还没加载完毕)
+      // 则等着我们页面的主要要素加载完毕再去设置body的字体大小.  document的DOMContentLoaded事件
       document.addEventListener('DOMContentLoaded', setBodyFontSize)
     }
   }
-  setBodyFontSize();
+  setBodyFontSize()
 
+  // 计算html节点字体大小 = html的宽度/10
   // set 1rem = viewWidth / 10
   function setRemUnit() {
     var rem = docEl.clientWidth / 20
@@ -21,15 +27,17 @@
 
   setRemUnit()
 
-  // reset rem unit on page resize
+  // 当页面大小发生变化时，重新设置rem的大小
   window.addEventListener('resize', setRemUnit)
+  // pageshow是我们重新加载页面时触发的事件
   window.addEventListener('pageshow', function (e) {
+    // 如果是从缓存中取过来的页面，也需要重新计算一下rem的大小
     if (e.persisted) {
       setRemUnit()
     }
   })
 
-  // detect 0.5px supports
+  // 解决有些移动端浏览器不支持0.5像素的写法
   if (dpr >= 2) {
     var fakeBody = document.createElement('body')
     var testElement = document.createElement('div')
@@ -41,4 +49,4 @@
     }
     docEl.removeChild(fakeBody)
   }
-}(window, document))
+})(window, document)
--- js/myMap.js
+++ js/myMap.js
@@ -1,6 +1,6 @@
-(function() {
+;(function () {
   // 1. 实例化对象
-  var myChart = echarts.init(document.querySelector(".geo"));
+  var myChart = echarts.init(document.querySelector('.geo'))
   // 2. 指定配置和数据
   var geoCoordMap = {
     上海: [121.4648, 31.2891],
@@ -117,69 +117,71 @@
     阳泉: [113.4778, 38.0951],
     青岛: [120.4651, 36.3373],
     韶关: [113.7964, 24.7028]
-  };
+  }
 
-  var XAData = [
-    [{ name: "西安" }, { name: "北京", value: 100 }],
-    [{ name: "西安" }, { name: "上海", value: 100 }],
-    [{ name: "西安" }, { name: "广州", value: 100 }],
-    [{ name: "西安" }, { name: "西宁", value: 100 }],
-    [{ name: "西安" }, { name: "拉萨", value: 100 }]
-  ];
+  var LSData = [
+    [{ name: '拉萨' }, { name: '贵阳', value: 100 }],
+    [{ name: '拉萨' }, { name: '西宁', value: 100 }]
+  ]
 
   var XNData = [
-    [{ name: "西宁" }, { name: "北京", value: 100 }],
-    [{ name: "西宁" }, { name: "上海", value: 100 }],
-    [{ name: "西宁" }, { name: "广州", value: 100 }],
-    [{ name: "西宁" }, { name: "西安", value: 100 }],
-    [{ name: "西宁" }, { name: "银川", value: 100 }]
-  ];
+    [{ name: '西宁' }, { name: '郑州', value: 100 }],
+    [{ name: '西宁' }, { name: '呼和浩特', value: 100 }]
+  ]
 
-  var YCData = [
-    [{ name: "拉萨" }, { name: "北京", value: 100 }],
-    [{ name: "拉萨" }, { name: "潍坊", value: 100 }],
-    [{ name: "拉萨" }, { name: "哈尔滨", value: 100 }]
-  ];
+  var ZZData = [
+    [{ name: '郑州' }, { name: '北京', value: 100 }],
+    [{ name: '郑州' }, { name: '青岛', value: 100 }],
+    [{ name: '郑州' }, { name: '哈尔滨', value: 100 }],
+    [{ name: '郑州' }, { name: '广州', value: 100 }],
+    [{ name: '郑州' }, { name: '上海', value: 100 }],
+    [{ name: '郑州' }, { name: '重庆', value: 100 }],
+    [{ name: '郑州' }, { name: '银川', value: 100 }],
+    [{ name: '郑州' }, { name: '厦门', value: 100 }],
+    [{ name: '郑州' }, { name: '拉萨', value: 100 }],
+    [{ name: '郑州' }, { name: '西宁', value: 100 }],
+    [{ name: '郑州' }, { name: '贵阳', value: 100 }]
+  ]
 
   var planePath =
-    "path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z";
+    'path://M1705.06,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705.06,1318.313z'
   //var planePath = 'arrow';
-  var convertData = function(data) {
-    var res = [];
+  var convertData = function (data) {
+    var res = []
     for (var i = 0; i < data.length; i++) {
-      var dataItem = data[i];
+      var dataItem = data[i]
 
-      var fromCoord = geoCoordMap[dataItem[0].name];
-      var toCoord = geoCoordMap[dataItem[1].name];
+      var fromCoord = geoCoordMap[dataItem[0].name]
+      var toCoord = geoCoordMap[dataItem[1].name]
       if (fromCoord && toCoord) {
         res.push({
           fromName: dataItem[0].name,
           toName: dataItem[1].name,
           coords: [fromCoord, toCoord],
           value: dataItem[1].value
-        });
+        })
       }
     }
-    return res;
-  };
+    return res
+  }
 
-  var color = ["#a6c84c", "#ffa022", "#46bee9"]; //航线的颜色
-  var series = [];
-  [
-    ["西安", XAData],
-    ["西宁", XNData],
-    ["银川", YCData]
-  ].forEach(function(item, i) {
+  var color = ['#a6c84c', '#ffa022', '#46bee9'] //航线的颜色
+  var series = []
+  ;[
+    ['拉萨', LSData],
+    ['西宁', XNData],
+    ['郑州', ZZData]
+  ].forEach(function (item, i) {
     series.push(
       {
-        name: item[0] + " Top3",
-        type: "lines",
+        name: item[0] + ' Top3',
+        type: 'lines',
         zlevel: 1,
         effect: {
           show: true,
           period: 6,
           trailLength: 0.7,
-          color: "red", //arrow箭头的颜色
+          color: 'red', //arrow箭头的颜色
           symbolSize: 3
         },
         lineStyle: {
@@ -192,10 +194,10 @@
         data: convertData(item[1])
       },
       {
-        name: item[0] + " Top3",
-        type: "lines",
+        name: item[0] + ' Top3',
+        type: 'lines',
         zlevel: 2,
-        symbol: ["none", "arrow"],
+        symbol: ['none', 'arrow'],
         symbolSize: 10,
         effect: {
           show: true,
@@ -215,66 +217,66 @@
         data: convertData(item[1])
       },
       {
-        name: item[0] + " Top3",
-        type: "effectScatter",
-        coordinateSystem: "geo",
+        name: item[0] + ' Top3',
+        type: 'effectScatter',
+        coordinateSystem: 'geo',
         zlevel: 2,
         rippleEffect: {
-          brushType: "stroke"
+          brushType: 'stroke'
         },
         label: {
           normal: {
             show: true,
-            position: "right",
-            formatter: "{b}"
+            position: 'right',
+            formatter: '{b}'
           }
         },
-        symbolSize: function(val) {
-          return val[2] / 8;
+        symbolSize: function (val) {
+          return val[2] / 8
         },
         itemStyle: {
           normal: {
             color: color[i]
           },
           emphasis: {
-            areaColor: "#2B91B7"
+            areaColor: '#2B91B7'
           }
         },
-        data: item[1].map(function(dataItem) {
+        data: item[1].map(function (dataItem) {
           return {
             name: dataItem[1].name,
             value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
-          };
+          }
         })
       }
-    );
-  });
+    )
+  })
   var option = {
     tooltip: {
-      trigger: "item",
-      formatter: function(params, ticket, callback) {
-        if (params.seriesType == "effectScatter") {
-          return "线路：" + params.data.name + "" + params.data.value[2];
-        } else if (params.seriesType == "lines") {
+      trigger: 'item',
+      formatter: function (params, ticket, callback) {
+        if (params.seriesType == 'effectScatter') {
+          return '线路：' + params.data.name + '' + params.data.value[2]
+        } else if (params.seriesType == 'lines') {
           return (
             params.data.fromName +
-            ">" +
+            '>' +
             params.data.toName +
-            "<br />" +
+            '<br />' +
             params.data.value
-          );
+          )
         } else {
-          return params.name;
+          return params.name
         }
       }
     },
 
     geo: {
-      map: "china",
+      map: 'china',
       label: {
         emphasis: {
           show: true,
-          color: "#fff"
+          color: '#fff'
         }
       },
       roam: true,
@@ -282,20 +284,21 @@
       zoom: 1.2,
       itemStyle: {
         normal: {
-          areaColor: "#142957",
-          borderColor: "#195BB9",
+          // 地图省份背景颜色
+          areaColor: 'rgba(20,41,87,0.3)',
+          borderColor: '#195BB9',
           borderWidth: 1
         },
         emphasis: {
-          areaColor: "#2B91B7"
+          areaColor: '#2B91B7'
         }
       }
     },
     series: series
-  };
+  }
   // 3. 把数据和配置给实例对象
-  myChart.setOption(option);
-  window.addEventListener("resize", function() {
-    myChart.resize();
-  });
-})();
+  myChart.setOption(option)
+  window.addEventListener('resize', function () {
+    myChart.resize()
+  })
+})()
